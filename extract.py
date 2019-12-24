f52b2ada83619d0b340f681c9119674319290906
Phil Butcher <ph.butcher@gmail.com>
2019-12-24 18:08:45 +0000

    timestamp filter on extract and render

--- extract.py
+++ extract.py
@@ -7,6 +7,9 @@ import base64
 from config import readerpwd
 
 limit = None
+year=2019
+starthour=9
+endhour=14
 
 if len(sys.argv) > 1:
 	limit = int(sys.argv[1])
@@ -17,9 +20,12 @@ else:
 db = MySQLdb.connect(host='localhost',user='Greenhouse.Reader',passwd=readerpwd, db='Greenhouse')
 dbc = db.cursor()
 
+filter = 'AND YEAR(ReadingTimestamp) = %d AND HOUR(ReadingTimestamp) >= %d AND HOUR(ReadingTimestamp) < %d' % (year, starthour, endhour)
+
 #start with the oldest photo
 
-sql = "SELECT ReadingPhoto, ReadingTimestamp FROM Greenhouse.Readings WHERE ReadingPhoto IS NOT NULL ORDER BY ReadingTimestamp ASC LIMIT 1;"
+sql = "SELECT ReadingPhoto, ReadingTimestamp FROM Greenhouse.Readings WHERE ReadingPhoto IS NOT NULL %s ORDER BY ReadingTimestamp ASC LIMIT 1;" % (filter,)
+#print (sql)
 dbc.execute(sql)
 rows = dbc.fetchall()
 
@@ -38,11 +44,12 @@ while (hasrow and (limit is None or index <= limit)):
 	print ("wrote %s to disk." % (filename,))
 
 	#continue with next oldest photo
-	
-	sql = "SELECT ReadingPhoto, ReadingTimestamp FROM Greenhouse.Readings WHERE ReadingPhoto IS NOT NULL AND ReadingTimestamp > '%s' ORDER BY ReadingTimestamp ASC LIMIT 1;" % (timestamp,)
+
+	sql = "SELECT ReadingPhoto, ReadingTimestamp FROM Greenhouse.Readings WHERE ReadingPhoto IS NOT NULL AND ReadingTimestamp > '%s' %s ORDER BY ReadingTimestamp ASC LIMIT 1;" % (timestamp,filter)
+	#print (sql)
 	dbc.execute(sql)
 	rows = dbc.fetchall()
-	
+
 	if len(rows) is 0:
 		hasrow = False
 	else:
